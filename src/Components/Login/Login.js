1c4af0776fef4112703454b27ba1f994f051f56a
Ohi20 <ashrafohi20@gmail.com>
2022-05-07 14:47:17 +0600

    login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
+            <div className='container w-50 mx-auto'>
             
+            <h2 className='text-primary text-center mt-2'>Please Login</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control  type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control  type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                    Login
+                </Button>
+            </Form>
+            
+            <p>New to Genius Car? <Link to="/register" className='text-info pe-auto text-decoration-none' >Please Register</Link> </p>
+            <p>Forget Password? <button className='btn btn-link text-info pe-auto text-decoration-none' >Reset Password</button> </p>
+            
+            
+        </div>
         </div>
     );
 };
