eaf7c54449b5cd33bc06be35140f8a080384fa35
Ohi20 <ashrafohi20@gmail.com>
2022-05-07 12:16:53 +0600

    added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Components/About/About';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import Services from './Components/Services/Services';
+
+function App() {
+  return (
+    <div className="App bg-dark">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<Services></Services>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+           <h2 className='text-info'>Website Title</h2>
+       <div className='container-fluid d-flex justify-content-between'>
+       <div>
+        <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="/">Navbar</Navbar.Brand>
+    <Nav className="me-auto navbar">
+      <Link to="/">Home</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blogs">Blogs</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </Nav>
+    </Container>
+  </Navbar>
+        </div>
+        <div>
+            <h2 className='text-info'>Hi</h2>
+        </div>
+       </div>
+  
+        </div>
+    );
+};
+
+export default Header;
